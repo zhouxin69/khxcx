dfc404b521dac27176632dfc235a5d60a7991f10
zhouxin69 <1007334240@qq.com>
2019-12-13 11:24:25 +0800

    first commit

---
+++ src/pages/member/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     22222222
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  },
+
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/shopping/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     1111111111111
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  },
+
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
